2e5c3625dff86d2a95632ebb73c4a20e2e77707b
chinmaya kumar behera <official.chinu2127@gmail.com>
2024-03-30 01:42:22 +0530

    add : cross origin * for backend

--- service/index.js
+++ service/index.js
@@ -21,6 +21,7 @@ const io = new Server(server, {
     origin: process.env.WEB_BASE_URL,
   },
 });
+io.origins("*:*");
 
 io.on("connection", (socket) => {
   console.log(`A user connected : ${socket.id}`);
